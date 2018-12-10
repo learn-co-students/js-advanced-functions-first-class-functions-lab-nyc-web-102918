daea0a7dfd483f19a92354aa1f654bacc769e003
vibhu mahendru <vibhumahendru@vibhus-MacBook-Pro.local>
2018-12-10 13:28:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  newAr = []
+  newAr = [drivers[0], drivers[1]]
+  return newAr
+}
+
+const returnLastTwoDrivers = function(drivers) {
+  newAr = []
+  newAr = [drivers[drivers.length -2], drivers[drivers.length-1]]
+  return newAr
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer) {
+  return function multiplier(fare){
+    return fare * integer
+  }
+
+}
+
+const fareDoubler =  createFareMultiplier(2)
+const fareTripler =  createFareMultiplier(3)
+
+function fetchSpecifiedDrivers(array, thing){
+  return (thing(array))
+}
